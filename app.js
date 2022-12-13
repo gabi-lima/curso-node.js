bbb2f05d994dfb58987532e6cb51eea60f4b6603
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-13 01:45:14 -0300

    Redirecionamento de Request, criando arquivo e armazenando dado

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const http = require("http");
-
+const fs = require("fs");
 const server = http.createServer(function (req, res) {
   const url = req.url;
+  const method = req.method;
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title> Minha primeira página! </title></head>");
-    res.write("<body> Estamos no caminho certo!</body>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name ='message'><button type='submit'>Enviar</button></form></body>"
+    );
     res.write("</html>");
-    res.end();
+    return res.end();
   }
   //console.log(req.url, req.method, req.headers);
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    res.statusCode = 302;
+    res.setHeader = ("Location", "/");
+    //res.writeHead("302, location", "/");
+    return res.end();
+  }
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
   res.write("<head><title> Minha primeira página! </title></head>");
