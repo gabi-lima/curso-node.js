3076cb03f546260f5efbcbc961926630355bded6
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-13 00:56:55 -0300

    criação de server, request e response

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const http = require("http");
 
-function rqListener(req, res) {}
-http.createServer();
+const server = http.createServer(function (req, res) {
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title> Minha primeira página! </title></head>");
+    res.write("<body> Estamos no caminho certo!</body>");
+    res.write("</html>");
+    res.end();
+  }
+  //console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> Minha primeira página! </title></head>");
+  res.write("<body> Estamos no caminho certo!</body>");
+  res.write("</html>");
+  res.end();
+});
+
+server.listen(3000);
