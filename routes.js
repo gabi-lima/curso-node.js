0abd541a94f45f4851213032f4a2d54a1190c27c
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-14 19:27:22 -0300

    fim do módulo introdutório de nodejs

---
+++ routes.js
@@ -0,0 +1,42 @@
+const fs = require("fs");
+
+function requestHandler(req, res) {
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title> Minha primeira página! </title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name ='message'><button type='submit'>Enviar</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  //console.log(req.url, req.method, req.headers);
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader = ("Location", "/");
+        return res.end();
+      });
+    });
+
+    //res.writeHead("302, location", "/");
+  }
+  res.setHeader("Content-type", "text/html");
+  res.write("<html>");
+  res.write("<head><title> Minha primeira página! </title></head>");
+  res.write("<body> Estamos no caminho certo!</body>");
+  res.write("</html>");
+  res.end();
+}
+
+module.exports = requestHandler;
