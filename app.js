ea7ea0d1657244f240a681e7dd014fe189b60597
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-15 01:52:50 -0300

    Utilizando body parser para analisar a data

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const http = require("http");
+const bodyParser = require("body-parser");
 const express = require("express");
+
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("another middleware");
+  res.send(
+    "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</form>"
+  );
+});
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
 app.use("/", (req, res, next) => {
   console.log("another middleware");
   res.send("Olá, do express!");
