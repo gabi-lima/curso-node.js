97760bd51b56081ecf823a60f303d65e934f1d05
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-15 01:26:48 -0300

    Começando módulo de Expressjs

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
 
-server.listen(3000);
+app.use("/", (req, res, next) => {
+  console.log("another middleware");
+  res.send("Olá, do express!");
+});
+
+app.listen(3000);
