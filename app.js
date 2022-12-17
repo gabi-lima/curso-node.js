85c1b6c67355378e0f8e12e164ea658dd838596e
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-16 21:34:38 -0300

    Adicionando páginas HTML ao projeto

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const rootDir = require("./util/path");
 const app = express();
 
 const adminRoutes = require("./routes/admin.js");
@@ -12,7 +13,7 @@ app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>page not found</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,10 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
+const rootDir = require("../util/path");
 
 router.get("/", (req, res, next) => {
-  res.send("Olá, do express!");
+  res.sendFile(path.join(rootDir, "views", "shop.html"));
 });
 
 module.exports = router;
