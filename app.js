a5173d02b8d69e793196fb984cb2a31dfeaf0164
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-20 22:00:11 -0300

    Introdução a templating

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
 const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const rootDir = require("./util/path");
+
 const app = express();
 
-const adminRoutes = require("./routes/admin.js");
+const adminData = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
