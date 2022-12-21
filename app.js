8269f4f853f7c31f297e99bb73148368fcfebf88
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-21 03:15:41 -0300

    usando PUG

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 const adminData = require("./routes/admin.js");
 const shopRoutes = require("./routes/shop.js");
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,7 +19,7 @@ app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { pageTitle: "Page not Found!" });
 });
 
 app.listen(3000);
