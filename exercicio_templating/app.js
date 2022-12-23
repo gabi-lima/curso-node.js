68c973ec333d5d384262979b96b41f8aa26552da
Gabriel Lima <gabriel1277lima@gmail.com>
2022-12-22 22:51:18 -0300

    exercicio

--- exercicio_templating/app.js
+++ exercicio_templating/app.js
@@ -2,18 +2,24 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-
-var myLogger = function (req, res, next) {
-  console.log("LOGGED");
-  next();
-};
+const user = [];
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use(myLogger);
 
 app.get("/", function (req, res) {
-  res.render("user");
+  res.render("user", { user: user, path: "/" });
+});
+
+app.post("/", (req, res, next) => {
+  user.push({ username: req.body.username });
+  console.log(user);
+  res.redirect("/users");
+});
+
+app.get("/users", (req, res, next) => {
+  res.render("users");
 });
 
 app.listen(3000);
